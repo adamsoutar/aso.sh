1eedc668c82915bed2e855d68905643fcdd130ac
Adam Soutar <adam@overflo.me>
2023-01-20 22:19:09 +0000

    Escape HTML entities in React source

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,10 @@ export default function Home() {
         <title>Adam Soutar</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
-      <h3>Hello, I'm Adam Soutar.</h3>
-      <p>I'm a Software Engineer. I write TypeScript, Rust, C# and Java.</p>
+      <h3>Hello, I&apos;m Adam Soutar.</h3>
+      <p>
+        I&apos;m a Software Engineer. I write TypeScript, Rust, C# and Java.
+      </p>
       <p>
         This extremely advanced world wide web site is a placeholder which
         mainly saves this domain for my email address.
