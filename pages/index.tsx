d555d316a541666cf31a23228363b2ef1be6da65
Adam Soutar <adam@overflo.me>
2023-01-20 22:12:49 +0000

    Initial commit
    Very basic placeholder site

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Adam Soutar</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <h3>Hello, I'm Adam Soutar.</h3>
+      <p>I'm a Software Engineer. I write TypeScript, Rust, C# and Java.</p>
+      <p>
+        This extremely advanced world wide web site is a placeholder which
+        mainly saves this domain for my email address.
+      </p>
+    </>
+  )
+}
